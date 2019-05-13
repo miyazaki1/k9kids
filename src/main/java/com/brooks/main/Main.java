668d62e2a23e8b8e995cbd122b82774ed847a587
keisuke_miyazaki <mint@momoirosoft.moe>
2019-05-12 23:25:44 -0400

    Merge branch 'development_revisions' of https://github.com/miyazaki1/k9kids into development.kei Fixed minor typo in Front Controller and fixed applicationContext bean...age

--- src/main/java/com/brooks/main/Main.java
+++ src/main/java/com/brooks/main/Main.java
@@ -1,20 +1,6 @@
 package com.brooks.main;
 
-import java.util.List;
-
 import org.apache.log4j.Logger;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
-import com.brooks.model.Canine;
-import com.brooks.model.Image;
-import com.brooks.service.AccountService;
-import com.brooks.service.DogService;
 
 public class Main {
 	public static Logger logger = Logger.getLogger(Main.class);
